3aa124ef6a7d0cdfa76463611500a6d7911b577f
Kung Örk <Kung rk@BERTIL>
2022-11-25 10:44:04 +0100

    Add project files.

---
+++ NumCounting/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Please write a number between 1 and 50.");
+bool Guessing = true;
+while (Guessing == true)
+{
+    Random randNum = new Random();
+    int numRes = randNum.Next(0, 100);
+    string uInput = Console.ReadLine();
+    
+    //if ()
+}
